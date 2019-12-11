51a227eb02d7f99b9c620e562984c731a05c4007
juanqui <juanquizh@gmail.com>
2019-12-11 14:01:48 -0500

    cambio de nombre al directorio

--- empresa/wsgi.py
+++ empresa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for clientes project.
+WSGI config for empresa project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clientes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empresa.settings')
 
 application = get_wsgi_application()
